0362e3a563325aaf09c52e8917446672938f2d4e
VincenzoCastrovilli <vcastrovilli@icloud.com>
2024-02-15 17:02:48 +0100

    Add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css';
+import ImageList from '../assets/images/photo-grid.png';
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <img src={ImageList} alt="List of experiences" className="hero-photo" />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-p">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
